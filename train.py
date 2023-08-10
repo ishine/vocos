cbe7082ca89ba509bd397d1c1c8c277ca8cd1684
tarepan <tarepan5884@gmail.com>
2023-08-10 15:14:15 +0000

    Add 1xA100 optimized configs

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
+import torch
 from pytorch_lightning.cli import LightningCLI
 
 
 if __name__ == "__main__":
+    torch.backends.cuda.matmul.allow_tf32 = True
+
     # Automatic instantiation
     cli = LightningCLI(run=False)
     cli.trainer.fit(model=cli.model, datamodule=cli.datamodule)
